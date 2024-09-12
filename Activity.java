d39ae641fb610e17e09068b2eb954843c671c956
BagunasJohnrey <johnreybagunas2@gmail.com>
2024-09-12 15:04:52 +0800

    first commit

---
+++ Activity.java
@@ -0,0 +1,12 @@
+class Activity{
+
+    public static void main(String args[]){
+
+        Dog dogObj = new Dog();
+
+        dogObj.name = "Chowlong";
+
+        dogObj.Puppies[0] = "aso1";
+
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,25 @@
+public class Dog {
+        String name;
+        boolean ismale;
+        int numofPuppies;
+        Dog[] Puppies = new Dog[5];
+        Dog Name;
+    
+        public void SetName(String Name){
+            
+        }
+        
+        public String showName(){
+            return name;
+        }
+
+        public int setNumOfPuppies(int numofPuppies){
+            return numofPuppies;
+        }
+
+        public void showNumberOfPuppies(){
+
+        }
+    
+
+}
