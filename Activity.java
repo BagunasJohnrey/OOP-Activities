f08d0021630a0e93a43164e4c011e38d4afa1f32
BagunasJohnrey <johnreybagunas2@gmail.com>
2024-09-20 09:21:13 +0800

    Update

--- Activity.java
+++ Activity.java
@@ -3,10 +3,50 @@ class Activity{
     public static void main(String args[]){
 
         Dog dogObj = new Dog();
+        Dog dogObj1 = new Dog();
 
-        dogObj.name = "Chowlong";
+        dogObj.set_Name("Chowlong");
+        System.out.println("Dog's Name: " + dogObj.showName());
 
-        dogObj.Puppies[0] = "aso1";
+        dogObj1.set_Name("Diosdado");
+        System.out.println("Dog's Name: " + dogObj1.showName());
+        
+        System.out.println(dogObj.is_Male = true);
+        System.out.println(dogObj1.is_Male = true);
+
+
+        dogObj.bark();
+        dogObj1.bark();
+
+        dogObj.set_Num_of_Puppies(7);
+        System.out.println(dogObj.show_Num_Of_Puppies());
+        dogObj.set_Num_of_Puppies(6);
+        System.out.println(dogObj.show_Num_Of_Puppies());
+
+        Dog[] aso1 = {
+            new Dog().set_Name("Bob"),
+            new Dog().set_Name("Kazumi"),
+            new Dog().set_Name("Xiaoyu"),
+            new Dog().set_Name("Dragunov"),
+            new Dog().set_Name("Gorlock the Destroyer"),
+            new Dog().set_Name("ron"),
+            new Dog().set_Name("nik")
+         };
+
+         Dog[] aso2 = {
+            new Dog().set_Name("Asuka"),
+            new Dog().set_Name("Lancelot"),
+            new Dog().set_Name("Hylos"),
+            new Dog().set_Name("Lolita"),
+            new Dog().set_Name("Hanabi"),
+            new Dog().set_Name("Layla")
+         };
+
+        dogObj.setPuppies(aso1);
+        dogObj.showList();
+
+        dogObj1.setPuppies(aso2);
+        dogObj1.showList();
 
     }
 }
--- Dog.java
+++ Dog.java
@@ -1,25 +1,42 @@
+
 public class Dog {
-        String name;
-        boolean ismale;
-        int numofPuppies;
-        Dog[] Puppies = new Dog[5];
-        Dog Name;
-    
-        public void SetName(String Name){
-            
-        }
-        
-        public String showName(){
-            return name;
-        }
+    String name;
+    boolean is_Male;
+    int num_of_Puppies;
+    public Dog[] list_of_Puppies;
+   
 
-        public int setNumOfPuppies(int numofPuppies){
-            return numofPuppies;
-        }
+   public Dog set_Name(String Puppies){
+         this.name = Puppies;
+         return this;
+    }
+    public String showName(){
+        return name;
+    }
 
-        public void showNumberOfPuppies(){
+    public void bark(){
+        System.out.println("Woof! Woof!");
+    }
 
+    public void set_Num_of_Puppies(int num_of_Puppies){
+        this.num_of_Puppies = num_of_Puppies;
+    }
+    public int show_Num_Of_Puppies(){
+        return num_of_Puppies;
+    }
+
+    public Dog setPuppies(Dog[] aso) {
+        num_of_Puppies = aso.length;
+        list_of_Puppies = aso;
+        return this;
+    }
+
+    public void showList(){
+        for (int i = 0; i < list_of_Puppies.length; i++) {
+            System.out.println(list_of_Puppies[i].name);
         }
-    
+    }
+
 
 }
+
